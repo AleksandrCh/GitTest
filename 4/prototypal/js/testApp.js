500422897dacfeedc2c96f4b8c4eab53ab67ce5f
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-07-25 10:47:03 +0300

    add test module

---
+++ 4/prototypal/js/testApp.js
@@ -0,0 +1,5 @@
+(function(global, undefined) {
+    'use strict';
+    var app = new global.TestModule();
+    app.test(global.Dot, global.Shape);
+})(this);
---
+++ 4/prototypal/js/testModul.js
@@ -0,0 +1,14 @@
+(function(global, undefined) {
+    'use strict';
+    global.TestModule = function() {
+    
+        var test = function(Child, Parent) {
+            var dot = new Dot('Dot', 1, 10);
+            console.log(dot.draw());
+        }
+    
+        return {
+            test: test
+        };
+    };
+})(this);
