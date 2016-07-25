aa47060c60963dc1a5ca9df178efbafd738265e1
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-07-25 11:34:58 +0300

    add test

--- 4/parasitic/js/testModule.js
+++ 4/parasitic/js/testModule.js
@@ -2,13 +2,44 @@
     'use strict';
     global.TestModule = function() {
     
-        var test = function(Child, Parent) {
-                        var dot = new Dot('Dot', 1, 10);
-            console.log(dot.draw());
+        var areEqual = function(functionName, expectedResult, actualResult) {
+            if (expectedResult === actualResult) {
+                console.log(functionName + ': success!');
+            } else {
+                console.log(functionName + ': failed! Expected result: ' + expectedResult + ', Actual result: ' + actualResult);
+            }
+        };
+
+        /*var getFunctionName = function(func) {
+            var name = argument.caller;
+            console.log(name);
+            name = name.substr('function '.length, name.indexOf('('));
+            name = name.trim();
+            console.log(name);
+            return name;
+        };*/
+
+        var dotTest = function() {
+            var dot = new Dot('Dot', 1, 10);
+
+            var expectedResult = 'Dot',
+            actualResult = dot.draw();
+
+            areEqual('dotTest', expectedResult, actualResult);
+        };
+
+        var circleTest = function() {
+            var circle = new Circle('Circle', 1, 10);
+
+            var expectedResult = 'Circle',
+                actualResult = circle.draw();
+
+            areEqual('circleTest', expectedResult, actualResult);
         }
     
         return {
-            test: test
+            dotTest: dotTest,
+            circleTest: circleTest
         };
     };
 })(this);
