329ed568683a452bac665dc9ec040f3bb81fbb68
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-07-29 15:00:42 +0300

    commit

--- 6/block/js/interfaceBlocker.js
+++ 6/block/js/interfaceBlocker.js
@@ -7,13 +7,34 @@
         interfaceBlockerText: 'interface-blocker-text'
     };
     
-    /*var init = function(config) {
-        var html ='<div class="interface-blocker-overlay"><div class="' + config.interfaceBlockerBox + '"><div class="' + config.interfaceBlockerText + '">' + config.text + '</div><div class="interface-blocker-button">OK</div></div></div>';
-        $('body').append(config);
-    }*/
-    
+    var init = function(config) {
+        if (!($('*').is('.interface-blocker-overlay'))) {
+            var html ='<div class="interface-blocker-overlay"><div class="' + config.interfaceBlockerBox + '"><div class="' + config.interfaceBlockerText + '">' + config.text + '</div><div class="interface-blocker-button">OK</div></div></div>';
+            
+            $('body').append(html);
+        
+            $('.interface-blocker-overlay').css({
+                'display': 'none',
+                'width': '100%',
+                'height': '100%',
+                'position': 'fixed',
+                'top': '0',
+                'bottom': '0',
+                'left': '0',
+                'right': '0',
+                'margin': 'auto',
+                'z-index': '50',
+                'background-color': 'rgba(0, 0, 0, 0.42)',
+                'transition': 'all 1s ease',
+                '-webkit-transition': 'all 2s 1s ease'
+            });
+        }
+    }
+        
     $.fn.interfaceBlocker = function(options) {
         var config = $.extend({}, defaults, options)
+        
+        init(config);
         
         this.click(function(e) {   
             $('.interface-blocker-overlay').css('display', 'block');
