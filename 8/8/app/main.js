9d0a43e4a5f6caccb557651de691bd2b9559c4ae
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-08-09 14:13:52 +0300

    c

---
+++ 8/8/app/main.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+
+})();
