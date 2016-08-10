dce4f5a5a534dd7681655b5609bc8810e9953a95
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-08-10 11:19:35 +0300

    add constructor style

--- 8/8/js/main.js
+++ 8/8/js/main.js
@@ -2,16 +2,16 @@
     var app = angular.module('app', ['ngRoute']);
 
     app.config(function ($routeProvider) {
-        $routeProvider.when('/pizza',
-        {
-            templateUrl: 'templates/readyPizza.html',
-            controller: 'pizzaController'
-        });
-        $routeProvider.when('/constructor',
-        {
-            templateUrl: 'templates/constructor.html',
-            controller: 'pizzaConstructorController'
-        });
+        $routeProvider
+            .when('/pizza', {
+                templateUrl: 'templates/readyPizza.html',
+                controller: 'pizzaController'
+            })
+            .when('/constructor', {
+                templateUrl: 'templates/constructor.html',
+                controller: 'pizzaConstructorController'
+            })
+            .otherwise({ redirectTo: '/pizza' })
     });
 
     app.controller('pizzaController', ['$scope', '$http', function ($scope, $http) {
@@ -22,6 +22,8 @@
     }]);
 
     app.controller('pizzaConstructorController', ['$scope', '$http', function ($scope, $http) {
-
+        $http.get('json/ingredients.json').success(function (data) {
+            $scope.ingredients = data;
+        });
     }]);
 })(this);
