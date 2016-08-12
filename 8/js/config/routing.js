ddc1d7d5af090a7c33389ac0659c61945277770d
Aliaksandr Chachotka <aliaksandr.chachotka@itechart-group.com>
2016-08-12 09:37:19 +0300

    add some changes

--- 8/js/config/routing.js
+++ 8/js/config/routing.js
@@ -7,25 +7,17 @@
                 controller: 'pizzaController'
             })
             .when('/constructor', {
-                templateUrl: '/templates/constructor.html',
+                templateUrl: 'templates/constructor.html',
                 controller: 'pizzaConstructorController'
             })
             .when('/orderHistory', {
-                templateUrl: '/templates/orderHistory.html',
+                templateUrl: 'templates/orderHistory.html',
                 controller: 'orderHistoryController'
             })
             .when('/cart', {
-                templateUrl: '/templates/cart.html',
+                templateUrl: 'templates/cart.html',
                 controller: 'cartController'
             })
             .otherwise({ redirectTo: '/pizza' });
-    });
-    
-    app.run(function($rootScope, $templateCache) {
-        $rootScope.$on('$routeChangeStart', function(event, next, current) {
-            if (typeof(current) !== 'undefined') {
-                $templateCache.remove(current.templateUrl);
-            }
-        })
     });
 })(app);
